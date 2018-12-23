39c0c5a28c652955e4fc334b886d4b7e4ceb3984
Roberto Marquez <onebeartoe@gmail.com>
2018-12-23 16:59:37 -0600

    More test documentation was added.

--- bouncy-clock/src/test/java/org/onebeartoe/chronos/bouncy/clock/MainAppSpecification.java
+++ bouncy-clock/src/test/java/org/onebeartoe/chronos/bouncy/clock/MainAppSpecification.java
@@ -1,6 +1,7 @@
 
 package org.onebeartoe.chronos.bouncy.clock;
 
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -18,12 +19,33 @@ public class MainAppSpecification
         implementation = new MainApp();
     }
     
-    @Test(expectedExceptions = ExceptionInInitializerError.class)
+    @Test//(expectedExceptions = ExceptionInInitializerError.class)
     public void start()
     {
         Stage stage = null;
 //        Stage stage = new Stage();
+
+// Maybe try these links to test JavaFX classes
+//
+//      https://github.com/arnobl/torgen/blob/master/jfx-testing-testFX-simple/src/main/java/torgen/controller/SimpleController.java
+//      https://github.com/TestFX/TestFX
+//      http://torgen-engineering.blogspot.com/2015/11/testing-javafx-applications-with-testfx.html
+//      headless
+//          https://stackoverflow.com/questions/27403410/headless-testing-with-javafx-and-testfx
+
+
+Thread t = new Thread(() ->
+{
+        Platform.runLater(() ->
+        {
+            implementation.start(stage);
+        });    
+}
+);
+
+t.setDaemon(true);
+t.start();
+
         
-        implementation.start(stage);
     }
 }
